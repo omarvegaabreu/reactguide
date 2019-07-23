dadc3369eeeb737dc493eca9af72ca9b6939f3f3
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-23 16:01:53 -0400

    understanding state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Omar", age: "37" },
+      { name: "Mildred", age: "63" },
+      { name: "Romulo", age: "63" }
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log("was clicked!");
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
-        <Person name="Omar" age="37">
+        <p>Learning react basics</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
           My hobbie is: Jujitsu!
         </Person>
-        <Person name="Jose" age="87" />
-        <Person name="Romulo" age="63" />
-        <Person name="Mildred" age="63" />
+
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
