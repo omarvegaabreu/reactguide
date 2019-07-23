43552bb0770b36f740e1cc0fc61717ef0eeedd1d
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-23 15:30:55 -0400

    understanding props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello world!</h1>
+        <Person name="Omar" age="37">
+          My hobbie is: Jujitsu!
+        </Person>
+        <Person name="Jose" age="87" />
+        <Person name="Romulo" age="63" />
+        <Person name="Mildred" age="63" />
       </div>
     );
   }
